cb1e073a6dd549ae99fb4e520ad65c995952d05a
coco875 <59367621+coco875@users.noreply.github.com>
2022-06-10 12:47:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
-from email.policy import strict
 import json
 import random
 
-with open("themedb-en.json", "r", encoding="utf-8") as f:
+with open("data.json", "r", encoding="utf-8") as f:
     data = json.load(f)
-
-len_max = 0
-for i in data:
-    if data[i].startswith("#") or data[i].startswith("(none)"):
-        continue
-    len_max = max(len_max, len(data[i].split(" ")))
-
-print(len_max)
 
 all_words = {}
 first_words = []
 
-for i in data:
+for i in (data if type(data) == dict else range(len(data))):
     if data[i].startswith("#"):
         continue
     l = data[i].split(" ")
     first_words.append(l[0])
-    for j in range(len(l), len_max):
-        l.append("")
     for j in range(len(l)-1):
         if l[j] not in all_words:
             all_words[l[j]] = []
         if l[j+1] != "":
             all_words[l[j]].append(l[j+1])
 
-#print(all_words)
-
 def generate_sentence():
     string = ""
     last_word = None
-    for i in range(len_max):
+    while True:
         if last_word in all_words:
             word = random.choice(all_words[last_word])
             string += word + " "
